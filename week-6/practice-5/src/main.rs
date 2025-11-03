ff68787b99bd60631dd10469c4e2575ed22e53a5
prinzkenny101 <kennedy.ekeozo@pau.edu.ng>
2025-11-03 15:50:41 +0100

    practice-5

---
+++ week-6/practice-5/src/main.rs
@@ -0,0 +1,14 @@
+fn main() 
+{
+
+    let fullname = "Pan-Atlantic Univeristy";
+    println!();
+    println!("Name: {}", fullname);
+    println!();
+    println!("Before trim");
+    println!("length is {}", fullname.len());
+    println!();
+    println!("After trim");
+    println!("length is {}", fullname.trim().len());
+
+}
