e1201ad0b742d11ec5b2ee676d0f821117e16ea8
prinzkenny101 <kennedy.ekeozo@pau.edu.ng>
2025-11-10 14:55:58 +0100

    practice-8

---
+++ week-7/practice-8/target/debug/deps/practice_8-0432ad40c96c751b.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\k.ekeozoCOS101\week-7\practice-8\target\debug\deps\practice_8-0432ad40c96c751b.d: src\main.rs
+
+C:\Users\New\Documents\k.ekeozoCOS101\week-7\practice-8\target\debug\deps\practice_8-0432ad40c96c751b.exe: src\main.rs
+
+src\main.rs:
