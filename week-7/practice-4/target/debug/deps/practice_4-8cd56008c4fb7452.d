84436b2a0e5b616e8b7c18c930a4284f9d439fdf
prinzkenny101 <kennedy.ekeozo@pau.edu.ng>
2025-11-10 14:06:16 +0100

    practice-4

---
+++ week-7/practice-4/target/debug/deps/practice_4-8cd56008c4fb7452.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\k.ekeozoCOS101\week-7\practice-4\target\debug\deps\practice_4-8cd56008c4fb7452.d: src\main.rs
+
+C:\Users\New\Documents\k.ekeozoCOS101\week-7\practice-4\target\debug\deps\practice_4-8cd56008c4fb7452.exe: src\main.rs
+
+src\main.rs:
