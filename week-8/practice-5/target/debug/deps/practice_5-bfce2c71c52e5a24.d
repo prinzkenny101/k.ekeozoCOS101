f1c1d2f9e7b5d40dc637f91f9a66456bccf3128b
prinzkenny101 <kennedy.ekeozo@pau.edu.ng>
2025-11-17 14:36:16 +0100

    practice-5

---
+++ week-8/practice-5/target/debug/deps/practice_5-bfce2c71c52e5a24.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\k.ekeozoCOS101\week-8\practice-5\target\debug\deps\practice_5-bfce2c71c52e5a24.d: src\main.rs
+
+C:\Users\New\Documents\k.ekeozoCOS101\week-8\practice-5\target\debug\deps\practice_5-bfce2c71c52e5a24.exe: src\main.rs
+
+src\main.rs:
