adf0407d2d07201688b6b09a39ba5925c37305ff
prinzkenny101 <kennedy.ekeozo@pau.edu.ng>
2025-12-01 14:03:18 +0100

    practice 6 - 9

---
+++ week-10/practice-7/target/debug/deps/practice_7-61a92fc399c80b45.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\k.ekeozoCOS101\week-10\practice-7\target\debug\deps\practice_7-61a92fc399c80b45.d: src\main.rs
+
+C:\Users\New\Documents\k.ekeozoCOS101\week-10\practice-7\target\debug\deps\practice_7-61a92fc399c80b45.exe: src\main.rs
+
+src\main.rs:
---
+++ week-10/practice-9/target/debug/deps/practice_9-e5f8e691c4d75f88.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\k.ekeozoCOS101\week-10\practice-9\target\debug\deps\practice_9-e5f8e691c4d75f88.d: src\main.rs
+
+C:\Users\New\Documents\k.ekeozoCOS101\week-10\practice-9\target\debug\deps\practice_9-e5f8e691c4d75f88.exe: src\main.rs
+
+src\main.rs:
